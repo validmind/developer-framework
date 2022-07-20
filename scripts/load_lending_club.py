4815c7eb6e1b99321b9bd56b59a484f84ffb4073
Andres Rodriguez <andres@certifiq.ai>
2022-07-19 17:11:16 -0700

    Update run_tests script

--- scripts/load_lending_club.py
+++ scripts/load_lending_club.py
@@ -19,9 +19,13 @@ import validmind as vm
 print("1. Initializing SDK...")
 
 # For test environment use api_host="https://api.test.vm.validmind.ai/api/v1/tracking"
+# Staging project ID=cl5qyzlup00001mmf5xox0ptp
 vm.init(
-    api_host="https://api.test.vm.validmind.ai/api/v1/tracking",
-    project="cl1jyvh2c000909lg1rk0a0zb",
+    # api_host="https://api.staging.vm.validmind.ai/api/v1/tracking",
+    # project="cl1jyvh2c000909lg1rk0a0zb",
+    project="cl5qyzlup00001mmf5xox0ptp",
+    api_key="ddeff6b7b68b6c6346dcfad49d62a2e2",
+    api_secret="dc7fb85e3aefd1675f68cfee091339a9c62a2f05097b89393a8d044228214188",
 )
 
 run_cuid = vm.start_run()
@@ -40,116 +44,116 @@ for content_id in content_ids:
     with open(f"./scripts/lending_club_metadata/{content_id}.md") as f:
         vm.log_metadata(content_id, f.read())
 
-# print("3. Loading dataset...")
+print("3. Loading dataset...")
 
-# df = pd.read_pickle("./notebooks/datasets/_temp/df_loans_cleaned.pickle")
+df = pd.read_pickle("./notebooks/datasets/_temp/df_loans_cleaned.pickle")
 # This dataset has been cleaned up much more after df_loans_cleaned
-# train_df = pd.read_pickle("./notebooks/datasets/_temp/df_loans_fe.pickle")
+train_df = pd.read_pickle("./notebooks/datasets/_temp/df_loans_fe.pickle")
 
-# targets = vm.DatasetTargets(
-#     target_column="loan_status",
-#     class_labels={
-#         "0": "Fully Paid",
-#         "1": "Charged Off",
-#     },
-# )
+targets = vm.DatasetTargets(
+    target_column="loan_status",
+    class_labels={
+        "0": "Fully Paid",
+        "1": "Charged Off",
+    },
+)
 
-# print("4. Logging dataset metadata and statistics...")
+print("4. Logging dataset metadata and statistics...")
 
-# vm_dataset = vm.log_dataset(train_df, "training", analyze=True, targets=targets)
-
-
-# print("5. Running data quality tests...")
-
-# results = vm.run_dataset_tests(
-#     train_df,
-#     dataset_type="training",
-#     vm_dataset=vm_dataset,
-#     send=True,
-#     run_cuid=run_cuid,
-# )
+vm_dataset = vm.log_dataset(train_df, "training", analyze=True, targets=targets)
 
 
-# print("6. Loading and preparing dataset for training...")
+print("5. Running data quality tests...")
 
-# COLS_CORRELATED = [
-#     "num_actv_rev_tl",
-#     "open_il_12m",
-#     "open_rv_12m",
-#     "avg_cur_bal",
-#     "num_bc_tl",
-#     "mo_sin_old_rev_tl_op",
-# ]
+results = vm.run_dataset_tests(
+    train_df,
+    dataset_type="training",
+    vm_dataset=vm_dataset,
+    send=True,
+    run_cuid=run_cuid,
+)
 
-# train_df.drop(COLS_CORRELATED, axis=1, inplace=True)
-# print("The following features  were removed.", COLS_CORRELATED)
 
-# df_categories = train_df.select_dtypes("object")
-# train_df = pd.get_dummies(
-#     train_df, columns=list(df_categories.columns), drop_first=False
-# )
+print("6. Loading and preparing dataset for training...")
 
-# print("7. Splitting dataset into training and validation sets...")
+COLS_CORRELATED = [
+    "num_actv_rev_tl",
+    "open_il_12m",
+    "open_rv_12m",
+    "avg_cur_bal",
+    "num_bc_tl",
+    "mo_sin_old_rev_tl_op",
+]
 
-# X, Y = train_df[train_df.columns.difference(["loan_status"])], train_df["loan_status"]
+train_df.drop(COLS_CORRELATED, axis=1, inplace=True)
+print("The following features  were removed.", COLS_CORRELATED)
 
-# X_train, X_test, y_train, y_test = train_test_split(
-#     X, Y, test_size=0.3, stratify=Y, random_state=10
-# )
+df_categories = train_df.select_dtypes("object")
+train_df = pd.get_dummies(
+    train_df, columns=list(df_categories.columns), drop_first=False
+)
 
-# train_ds, val_ds = train_test_split(train_df, test_size=0.3)
+print("7. Splitting dataset into training and validation sets...")
 
-# # For training
-# X_train = train_ds.drop("loan_status", axis=1)
-# y_train = train_ds.loc[:, "loan_status"].astype(int)
-# X_test = val_ds.drop("loan_status", axis=1)
-# y_test = val_ds.loc[:, "loan_status"].astype(int)
+X, Y = train_df[train_df.columns.difference(["loan_status"])], train_df["loan_status"]
 
-# print("8. Balancing training dataset...")
+X_train, X_test, y_train, y_test = train_test_split(
+    X, Y, test_size=0.3, stratify=Y, random_state=10
+)
 
-# X_train_subset = X_train[:1000]
-# y_train_subset = y_train[:1000]
-# pca = PCA(n_components=2)
-# X_train_subset = pca.fit_transform(X_train_subset)
+train_ds, val_ds = train_test_split(train_df, test_size=0.3)
 
-# over = SMOTE(sampling_strategy=0.5, k_neighbors=10)
-# under = RandomUnderSampler(sampling_strategy=1.0)
-# X_train_subset_o, y_train_subset_o = over.fit_resample(X_train_subset, y_train_subset)
-# X_train_subset_o_u, y_train_subset_o_u = under.fit_resample(
-#     X_train_subset_o, y_train_subset_o
-# )
+# For training
+X_train = train_ds.drop("loan_status", axis=1)
+y_train = train_ds.loc[:, "loan_status"].astype(int)
+X_test = val_ds.drop("loan_status", axis=1)
+y_test = val_ds.loc[:, "loan_status"].astype(int)
 
-# X_train, y_train = over.fit_resample(X_train, y_train)
-# X_train, y_train = under.fit_resample(X_train, y_train)
+print("8. Balancing training dataset...")
 
-# print("9. Training model...")
+X_train_subset = X_train[:1000]
+y_train_subset = y_train[:1000]
+pca = PCA(n_components=2)
+X_train_subset = pca.fit_transform(X_train_subset)
 
-# xgb_model = xgb.XGBClassifier(
-#     early_stopping_rounds=10,
-#     # n_estimators=5,
-# )
-# xgb_model.set_params(
-#     eval_metric=["error", "logloss", "auc"],
-# )
-# xgb_model.fit(
-#     X_train,
-#     y_train,
-#     eval_set=[(X_train, y_train), (X_test, y_test)],
-# )
+over = SMOTE(sampling_strategy=0.5, k_neighbors=10)
+under = RandomUnderSampler(sampling_strategy=1.0)
+X_train_subset_o, y_train_subset_o = over.fit_resample(X_train_subset, y_train_subset)
+X_train_subset_o_u, y_train_subset_o_u = under.fit_resample(
+    X_train_subset_o, y_train_subset_o
+)
 
-# print("10. Logging model parameters and training metrics...")
+X_train, y_train = over.fit_resample(X_train, y_train)
+X_train, y_train = under.fit_resample(X_train, y_train)
 
-# vm.log_model(xgb_model)
-# vm.log_training_metrics(xgb_model, X_train, y_train, run_cuid=run_cuid)
+print("9. Training model...")
 
-# y_pred = xgb_model.predict_proba(X_test)[:, -1]
-# predictions = [round(value) for value in y_pred]
-# accuracy = accuracy_score(y_test, predictions)
+xgb_model = xgb.XGBClassifier(
+    early_stopping_rounds=10,
+    # n_estimators=5,
+)
+xgb_model.set_params(
+    eval_metric=["error", "logloss", "auc"],
+)
+xgb_model.fit(
+    X_train,
+    y_train,
+    eval_set=[(X_train, y_train), (X_test, y_test)],
+)
 
-# print(f"Accuracy: {accuracy}")
+print("10. Logging model parameters and training metrics...")
 
-# print("11. Running model evaluation tests...")
+vm.log_model(xgb_model)
+vm.log_training_metrics(xgb_model, X_train, y_train, run_cuid=run_cuid)
 
-# eval_results = vm.run_model_tests(
-#     xgb_model, X_test, y_test, send=True, run_cuid=run_cuid
-# )
+y_pred = xgb_model.predict_proba(X_test)[:, -1]
+predictions = [round(value) for value in y_pred]
+accuracy = accuracy_score(y_test, predictions)
+
+print(f"Accuracy: {accuracy}")
+
+print("11. Running model evaluation tests...")
+
+eval_results = vm.run_model_tests(
+    xgb_model, X_test, y_test, send=True, run_cuid=run_cuid
+)
