c37491b0fab8c415641f3f502e89fb8b0670b0bf
Andres Rodriguez <andres@certifiq.ai>
2022-09-08 18:03:48 -0700

    WIP - add new high level wrappers

--- validmind/__init__.py
+++ validmind/__init__.py
@@ -19,7 +19,48 @@ from .model_evaluation import evaluate_model
 from .tests import run_dataset_tests
 from .tests.config import TestResult, TestResults
 
+# High Level API Wrappers
+# -----------------------
+# These functions are wrappers around the lower level functions that map directly
+# to an HTTP API call. We'll be slowly separating the Python API from the HTTP API
+
+
+def analyze_dataset(
+    dataset,
+    dataset_type,
+    dataset_options=None,
+    targets=None,
+    features=None,
+    send=True,
+):
+    """
+    Analyzes a dataset by extracting summary statistics and running data quality tests
+    on it. Results are logged to the ValidMind API
+
+    :param pd.DataFrame dataset: We only support Pandas DataFrames at the moment
+    :param str dataset_type: The dataset type is necessary for mapping and relating multiple datasets together.
+        Can be one of training, validation, test or generic
+    :param dict dataset_options: A dictionary of options for the dataset
+    :param vm.vm.DatasetTargets targets: A list of target variables
+    :param list features: An optional list of extra attributes for any feature
+    :param bool send: Whether to post the test results to the API. send=False is useful for testing
+    """
+    print("Analyzing dataset...")
+    vm_dataset = log_dataset(dataset, dataset_type, targets=targets, features=features)
+
+    print("Running data quality tests...")
+    results = run_dataset_tests(
+        dataset=dataset,
+        dataset_type=dataset_type,
+        vm_dataset=vm_dataset,
+        send=send,
+    )
+
+    return results
+
+
 __all__ = [
+    "analyze_dataset",
     "evaluate_model",
     "init",
     "log_dataset",
