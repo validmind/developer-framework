217da9b59d493c04011b4a8ce9fdc752d82a3a9c
John Walz <johnwalz97@gmail.com>
2023-05-19 18:16:03 -0400

    added global config

--- validmind/vm_models/test_plan.py
+++ validmind/vm_models/test_plan.py
@@ -32,6 +32,8 @@ class TestPlan:
 
     # Instance Variables
     config: dict() = None
+    _global_config: dict() = None
+    _test_configs: dict() = None
     test_context: TestContext = None
     # Stores a reference to the child test plan instances
     _test_plan_instances: List[object] = None
@@ -69,15 +71,23 @@ class TestPlan:
 
         self.validate_context()
 
-    def _merge_config(self, config):
-        """Merges global config with individual test configs if they exist"""
+        self._split_configs()
+
+    def _split_configs(self):
+        """Splits the config into a global config and test configs"""
         if self.config is None:
-            return config
+            return
 
-        if config is None:
-            return self.config
+        self._global_config = {}
+        self._test_configs = {}
 
-        return {**self.config, **config}
+        test_names = [test.name for test in self.tests]
+
+        for key, value in self.config.items():
+            if key in test_names:
+                self._test_configs[key] = value
+            else:
+                self._global_config[key] = value
 
     def title(self):
         """
@@ -128,7 +138,10 @@ class TestPlan:
         if self.config is None:
             return None
 
-        return self.config.get(test_name, None)
+        return {
+            **self._global_config,
+            **self._test_configs.get(test_name, {}),
+        }  # merges the global config with the test config
 
     def _init_pbar(self, render_summary: bool = True, send: bool = True):
         """
@@ -177,14 +190,12 @@ class TestPlan:
             self._init_pbar(render_summary=render_summary, send=send)
 
         for test in self.tests:
-            test_name = test.name
-
-            test_params = self.get_config_params_for_test(test_name)
-            test_instance = test(self.test_context, params=test_params)
-
-            self.pbar_description.value = (
-                f"Running {test.test_type}: {test_instance.name}"
+            test_instance = test(
+                test_context=self.test_context,
+                params=self.get_config_params_for_test(test.name),
             )
+
+            self.pbar_description.value = f"Running {test.test_type}: {test.name}"
 
             test_instance.run()
 
@@ -206,6 +217,7 @@ class TestPlan:
 
         # TODO: remove
         for test_plan in self.test_plans:
+            print(f"here... {test_plan}")
             test_plan_instance = test_plan(
                 config=self.config,
                 test_context=self.test_context,
