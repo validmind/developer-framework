0b3aa5c1d271bc5f2b4878dfb3a002fe142a65e2
John Walz <johnwalz97@gmail.com>
2023-05-21 23:31:44 -0400

    quick fix for test suite not finishing

--- validmind/utils.py
+++ validmind/utils.py
@@ -254,21 +254,10 @@ def run_async(func, *args, name=None, **kwargs):
     """
     try:
         if asyncio.get_event_loop().is_running() and is_notebook():
-            try:
-                from google.colab._shell import Shell
-
-                if isinstance(getipython.get_ipython(), Shell):
-                    # hacky way to make async code run "sync" in colab
-                    # this will make the progress bar work properly
-                    import nest_asyncio  # noqa: F401
-
-                    loop = asyncio.new_event_loop()
-                    nest_asyncio.apply(loop)
-                    res = loop.run_until_complete(func(*args, **kwargs))
-                    loop.close()
-                    return res
-            except ModuleNotFoundError:
-                pass
+            if __loop:
+                future = __loop.create_task(func(*args, **kwargs), name=name)
+                # wait for the future result
+                return __loop.run_until_complete(future)
 
             return asyncio.get_event_loop().create_task(
                 func(*args, **kwargs), name=name
