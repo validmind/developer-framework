ba734da5fae193ecc000612d859ad40aeb6757d8
Juan <juan@validmind.ai>
2023-06-08 15:15:48 +0100

    Add LoanDefaultRatio metric

--- validmind/data_validation/metrics.py
+++ validmind/data_validation/metrics.py
@@ -414,7 +414,6 @@ class TabularDateTimeHistograms(Metric):
         )
 
 
-@dataclass
 class TabularDescriptionTables(Metric):
     """
     Collects a set of descriptive statistics for a tabular dataset, for
@@ -559,6 +558,74 @@ class TabularDescriptionTables(Metric):
                 "categorical": summary_stats_categorical,
                 "datetime": summary_stats_datetime,
             }
+        )
+
+
+class LoanDefaultRatio(Metric):
+    """
+    Generates a visual analysis of loan default ratios by plotting bar plots.
+    The input dataset can have multiple categorical variables if necessary.
+    In this case, we produce a separate plot for each categorical variable.
+    """
+
+    name = "loan_default_ratio"
+    required_context = ["dataset"]
+    default_params = {"loan_status_col": None, "columns": None}
+
+    def plot_loan_default_ratio(self, loan_status_col, columns=None, rsorted=True):
+        df = self.dataset.df
+
+        # Use all features if columns is not specified, else use selected columns
+        features = df.columns if columns is None else columns
+
+        figures = []
+        for feature in features:
+            fig, axs = plt.subplots(1, 2)
+
+            # Create sorted unique list of feature values for both plots
+            feature_dimension = (
+                sorted(df[feature].unique()) if rsorted else df[feature].unique()
+            )
+
+            # First subplot for univariate count
+            count_values = [
+                df[df[feature] == fd][loan_status_col].count()
+                for fd in feature_dimension
+            ]
+            axs[0].bar(feature_dimension, count_values, color="#6699cc")
+            axs[0].set_title(f"{feature}", fontsize=18)
+            axs[0].set_ylabel("Count", fontsize=18)
+
+            # Second subplot for univariate ratio
+            ratio_values = [
+                df[df[feature] == fd][loan_status_col].mean()
+                for fd in feature_dimension
+            ]
+            axs[1].bar(feature_dimension, ratio_values, color="orange")
+            axs[1].set_title(f"{feature}", fontsize=18)
+            axs[1].set_ylabel("Loan Defaults Ratio", fontsize=18)
+
+            figures.append(
+                Figure(
+                    for_object=self,
+                    key=f"{self.key}:{feature}",
+                    figure=fig,
+                )
+            )
+
+        plt.tight_layout()
+        plt.close("all")
+
+        return self.cache_results(
+            figures=figures,
+        )
+
+    def run(self):
+        loan_status_col = self.params["loan_status_col"]
+        columns = self.params["columns"]
+
+        return self.plot_loan_default_ratio(
+            loan_status_col=loan_status_col, columns=columns
         )
 
 
