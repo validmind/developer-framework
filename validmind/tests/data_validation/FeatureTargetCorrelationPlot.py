9a97a1dfae76bd14f69a0c5de697ab19a145f6ce
Juan <juan@validmind.ai>
2023-07-04 20:41:05 +0100

    Modify FeatureTargetCorrelationPlot for plotly

--- validmind/tests/data_validation/FeatureTargetCorrelationPlot.py
+++ validmind/tests/data_validation/FeatureTargetCorrelationPlot.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-
+import plotly.graph_objects as go
+import numpy as np
 from dataclasses import dataclass
 from validmind.vm_models import Figure, Metric
 
@@ -14,67 +13,63 @@ class FeatureTargetCorrelationPlot(Metric):
 
     name = "feature_target_correlation_plot"
     required_context = ["dataset"]
-    default_params = {"features": None, "declutter": False, "fontsize": 13}
+    default_params = {"features": None, "fig_height": 600}
 
     def run(self):
         features = self.params["features"]
+        fig_height = self.params["fig_height"]
 
         if features is None:
             features = self.dataset.df.columns.to_list()
         else:
             features = self.params["features"]
 
-        declutter = self.params["declutter"]
-        fontsize = self.params["fontsize"]
         target_column = self.dataset.target_column
 
         # Filter DataFrame based on features and target_column
         df = self.dataset.df[features + [target_column]]
 
         figure = self.visualize_feature_target_correlation(
-            df, target_column, declutter, fontsize
+            df, target_column, fig_height
         )
 
         return self.cache_results(figures=figure)
 
-    def visualize_feature_target_correlation(
-        self, df, target_column, declutter, fontsize
-    ):
+    def visualize_feature_target_correlation(self, df, target_column, fig_height):
         # Compute correlations with the target variable
-        correlations = df.corr().iloc[:, 0].drop(target_column)
+        correlations = df.corr(numeric_only=True)[target_column].drop(target_column)
+        correlations = correlations.loc[:, ~correlations.columns.duplicated()]
 
-        # Sort correlations in descending order based on the first column
-        correlations = correlations.sort_values(ascending=False)
+        correlations = correlations.sort_values(by=target_column, ascending=True)
 
-        # Create the bar plot with adjusted width and ordered bars
-        fig, ax = plt.subplots()
-        sns.barplot(
-            x=correlations.values,
-            y=correlations.index,
-            palette="coolwarm_r",
-            ax=ax,
+        # Create a gradual color map from red (1) to blue (-1)
+        color_map = np.linspace(1, -1, len(correlations))
+        colors = [
+            f"rgb({int(255 * (1 - val))}, 0, {int(255 * (1 + val))})"
+            for val in color_map
+        ]
+
+        # Round the correlation values and convert to a list
+        rounded_correlations = correlations.values.round(2).tolist()
+
+        # Create a horizontal bar plot with gradual color mapping
+        fig = go.Figure(
+            data=go.Bar(
+                x=correlations[target_column],
+                y=correlations.index,
+                orientation="h",
+                marker=dict(color=colors),
+                hovertemplate="Feature: %{y}<br>Correlation: %{x:.2f}<extra></extra>",  # Hover template for tooltips
+            )
         )
 
-        plt.xticks(fontsize=fontsize)
-        plt.yticks(fontsize=fontsize)
-
-        plt.xlabel(None)
-        plt.ylabel(None)
-        plt.title(
-            f"Correlation of Features vs Target Variable ({target_column})",
-            fontsize=fontsize,
+        # Set the title and axis labels
+        fig.update_layout(
+            title=f"Correlations of Features vs Target Variable ({target_column})",
+            xaxis_title="",
+            yaxis_title="",
+            height=fig_height,  # Adjust the height value as needed
         )
-
-        plt.tight_layout()
-
-        if declutter:
-            ax.set_yticklabels([])
-            plt.ylabel(f"{len(correlations)} Numerical Features", fontsize=fontsize)
-        else:
-            for i, v in enumerate(correlations.values):
-                ax.text(v + 0.01, i, str(round(v, 2)), va="center", fontsize=fontsize)
-
-        plt.close("all")
 
         figure = Figure(for_object=self, key=self.key, figure=fig)
         return [figure]
