bfb3d35306e6c3a172e340036816ef228e743dab
John Walz <john@validmind.ai>
2023-08-21 16:34:10 -0400

    fixing error messages for when tests fail to run

--- validmind/vm_models/test_plan.py
+++ validmind/vm_models/test_plan.py
@@ -274,7 +274,9 @@ class TestPlan:
                 logger=logger,
             )()  # this is a decorator so we need to call it
         except Exception as e:
-            logger.error(f"Failed to run test '{test.name}': {e}")
+            logger.error(
+                f"Failed to run test '{test.name}': ({e.__class__.__name__}) {e}"
+            )
             test.result = TestPlanFailedResult(
                 name=f"Failed {test.test_type}",
                 error=e,
