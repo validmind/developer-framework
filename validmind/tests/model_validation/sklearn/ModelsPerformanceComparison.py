65fe435bb5257b0b7f9105917bac5333afca26f2
Anil Sorathiya <anil@validmind.ai>
2023-09-05 15:24:05 +0100

    Update hugging face demo notebook

--- validmind/tests/model_validation/sklearn/ModelsPerformanceComparison.py
+++ validmind/tests/model_validation/sklearn/ModelsPerformanceComparison.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 
 from .ClassifierPerformance import ClassifierPerformance
 from numpy import unique
+import pandas as pd
 from validmind.utils import format_number
 from validmind.vm_models import ResultSummary, ResultTable
 
@@ -11,7 +12,7 @@ from validmind.vm_models import ResultSummary, ResultTable
 @dataclass
 class ModelsPerformanceComparison(ClassifierPerformance):
     """
-    Test that outputs the performance of the model on the training data.
+    Test that outputs the models performance comparison on the test data.
     """
 
     name = "models_performance_comparison"
@@ -19,9 +20,9 @@ class ModelsPerformanceComparison(ClassifierPerformance):
 
     def description(self):
         return """
-        This section shows the performance of the model on the training data. Popular
+        This section shows the models performance comparison on the training data. Popular
         metrics such as the accuracy, precision, recall, F1 score, etc. are
-        used to evaluate the model.
+        used to evaluate the models.
         """
 
     def y_true(self):
@@ -31,29 +32,16 @@ class ModelsPerformanceComparison(ClassifierPerformance):
         return self.model.y_train_predict
 
     def summary(self, metric_value: dict):
-        # Turns the metric value into a table of [{metric_name: value}]
-        print(metric_value)
-        summary_out_sample_performance = []
-        for model_id, model_value in metric_value.iteritems():
-            for metric, value in metric_value[model_id].iteritems():
-                print(metric, value)
-        
-        for metric_name, metric_value in metric_value.items():
-            summary_out_sample_performance.append(
-                {
-                    "Metric": metric_name.title(),
-                    "Definition": self.metric_definitions[metric_name].format(
-                        target_column=self.model.train_ds.target_column
-                    ),
-                    "Formula": self.metric_formulas[metric_name],
-                    "Value": format_number(metric_value),
-                }
-            )
-        print("summary_out_sample_performance", summary_out_sample_performance)
+        df = pd.DataFrame(metric_value)
+        df.index.name = 'metric'
+        df = df.reset_index()
+        df["definition"] = df.metric.apply(lambda m: self.metric_definitions[m].format(
+            target_column=self.model.train_ds.target_column))
+        df["formula"] = df.metric.apply(lambda m: self.metric_formulas[m])
         return ResultSummary(
             results=[
                 ResultTable(
-                    data=summary_out_sample_performance
+                    data=df
                 ),
             ]
         )
@@ -68,17 +56,13 @@ class ModelsPerformanceComparison(ClassifierPerformance):
 
         if self.models is not None:
             all_models.extend(self.models)
-
-        print(all_models)
         results = {}
         m = 0
         for model in all_models:
-                
             y_true = model.y_test_true
             class_pred = model.y_test_predict
             result = {}
             metrics = self.params.get("metrics", self.default_params["metrics"])
-            print("metrics", metrics)
             for metric_name in metrics:
                 if metric_name not in self.default_metrics:
                     raise ValueError(f"Metric {metric_name} not supported.")
@@ -89,13 +73,11 @@ class ModelsPerformanceComparison(ClassifierPerformance):
                     or metric_name == "recall"
                     or metric_name == "f1"
                 ):
-                    result[metric_name] = metric_func(
-                        y_true, class_pred, pos_label=unique(y_true)[0]
+                    result[metric_name] = format_number(metric_func(
+                        y_true, class_pred, pos_label=unique(y_true)[0])
                     )
                 else:
-                    result[metric_name] = metric_func(y_true, class_pred)
-            result["model_name"] = "model_" + str(m)
-            results[ "model_" + str(m)] = result
+                    result[metric_name] = format_number(metric_func(y_true, class_pred))
+            results["model_" + str(m)] = result
             m += 1
-        print(results)
-        return self.cache_results(results)
+        return self.cache_results(pd.DataFrame(results))
