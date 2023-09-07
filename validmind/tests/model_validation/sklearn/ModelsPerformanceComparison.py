01662cf6334b0c9821aeb074449e8534dfc74ad0
Anil Sorathiya <anil@validmind.ai>
2023-09-07 11:49:36 +0100

    Use base class  "ClassifierPerformance" to compute performance metrics

--- validmind/tests/model_validation/sklearn/ModelsPerformanceComparison.py
+++ validmind/tests/model_validation/sklearn/ModelsPerformanceComparison.py
@@ -1,38 +1,20 @@
 # Copyright © 2023 ValidMind Inc. All rights reserved.
 
+from .ClassifierPerformance import ClassifierPerformance, multiclass_roc_auc_score
 from dataclasses import dataclass
-from functools import partial
 from numpy import unique
-import pandas as pd
-from sklearn import metrics, preprocessing
-from validmind.utils import format_number
-from validmind.vm_models import Metric, ResultSummary, ResultTable
-
-
-def multiclass_roc_auc_score(y_test, y_pred, average="macro"):
-    lb = preprocessing.LabelBinarizer()
-    lb.fit(y_test)
-    y_test = lb.transform(y_test)
-    y_pred = lb.transform(y_pred)
-    return metrics.roc_auc_score(y_test, y_pred, average=average)
+from sklearn import metrics
+from validmind.vm_models import ResultSummary, ResultTable, ResultTableMetadata
 
 
 @dataclass
-class ModelsPerformanceComparison(Metric):
+class ModelsPerformanceComparison(ClassifierPerformance):
     """
     Test that outputs the models performance comparison on the test data.
     """
 
     name = "models_performance_comparison"
     required_inputs = ["model", "models", "model.test_ds"]
-    default_params = {"metrics": ["accuracy", "precision", "recall", "f1", "roc_auc"]}
-    default_metrics = {
-        "accuracy": metrics.accuracy_score,
-        "precision": partial(metrics.precision_score, zero_division=0, average="micro"),
-        "recall": partial(metrics.recall_score, zero_division=0, average="micro"),
-        "f1": partial(metrics.f1_score, zero_division=0, average="micro"),
-        "roc_auc": partial(multiclass_roc_auc_score),
-    }
 
     def description(self):
         return """
@@ -47,21 +29,54 @@ class ModelsPerformanceComparison(Metric):
     def y_pred(self):
         return self.model.y_train_predict
 
-    def summary(self, metric_value: dict):
-        df = pd.DataFrame(metric_value)
-        df.index.name = 'metric'
-        df = df.reset_index()
-
-        return ResultSummary(
-            results=[
+    def binary_summary(self, metric_value: dict):
+        """
+        When building a binary classification summary for all the models. We take the positive class
+        metrics as the global metrics.
+        """
+        results = []
+        for m, m_v in metric_value.items():
+            result = super().binary_summary(m_v)
+            results.append(
                 ResultTable(
-                    data=df.to_dict("records")
-                ),
-            ]
-        )
+                    data=result.results[0].data,
+                    metadata=ResultTableMetadata(title=f"{result.results[0].metadata.title}: {m}"),
+                )
+            )
+        return ResultSummary(results=results)
+
+    def multiclass_summary(self, metric_value: dict):
+        """
+        The multi-class summary method that calculates weighted average,
+        macro average and per class metrics of all the models.
+        """
+        results = []
+        for m, m_v in metric_value.items():
+            result = super().multiclass_summary(m_v)
+            results.append(
+                ResultTable(
+                    data=result.results[0].data,
+                    metadata=ResultTableMetadata(title=f"{result.results[0].metadata.title}: {m}"),
+                )
+            )
+            results.append(
+                ResultTable(
+                    data=result.results[1].data,
+                    metadata=ResultTableMetadata(title=f"{result.results[1].metadata.title}: {m}"),
+                )
+            )
+        return ResultSummary(results=results)
+
+    def summary(self, metric_value: dict):
+        """
+        This summary varies depending if we're evaluating a binary or multi-class model
+        """
+        if len(unique(self.y_true())) > 2:
+            return self.multiclass_summary(metric_value)
+
+        return self.binary_summary(metric_value)
 
     def run(self):
-
         # Check models list is not empty
         if not self.models:
             raise ValueError("List of models must be provided in the models parameter to compare")
@@ -71,27 +86,10 @@ class ModelsPerformanceComparison(Metric):
         if self.models is not None:
             all_models.extend(self.models)
         results = {}
-        m = 0
-        for model in all_models:
+        for idx, model in enumerate(all_models):
             y_true = model.y_test_true
             class_pred = model.y_test_predict
-            result = {}
-            metrics = self.params.get("metrics", self.default_params["metrics"])
-            for metric_name in metrics:
-                if metric_name not in self.default_metrics:
-                    raise ValueError(f"Metric {metric_name} not supported.")
-                metric_func = self.default_metrics[metric_name]
-                y_true = y_true.astype(class_pred.dtype)
-                if (
-                    metric_name == "precision"
-                    or metric_name == "recall"
-                    or metric_name == "f1"
-                ):
-                    result[metric_name] = format_number(metric_func(
-                        y_true, class_pred, pos_label=unique(y_true)[0])
-                    )
-                else:
-                    result[metric_name] = format_number(metric_func(y_true, class_pred))
-            results["model_" + str(m)] = result
-            m += 1
+            report = metrics.classification_report(y_true, class_pred, output_dict=True)
+            report["roc_auc"] = multiclass_roc_auc_score(y_true, class_pred)
+            results["model_" + str(idx)] = report
         return self.cache_results(results)
