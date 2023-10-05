c99e2c964cf958a268dd9c07751eba02f02d3d35
Juan <juan@validmind.ai>
2023-10-05 18:43:27 +0200

    Add SummarizationPredictions metric

---
+++ validmind/tests/model_validation/SummarizationPredictions.py
@@ -0,0 +1,68 @@
+# Copyright © 2023 ValidMind Inc. All rights reserved.
+
+from dataclasses import dataclass
+
+import pandas as pd
+
+from validmind.vm_models import Metric, ResultSummary, ResultTable
+
+
+@dataclass
+class SummarizationPredictions(Metric):
+    """
+    Description for Summarization Predictions
+    """
+
+    name = "summarization_predictions"
+    default_params = {"display_limit": 5}
+    required_inputs = ["model"]
+    metadata = {
+        "task_types": ["text_summarization"],
+        "tags": ["summarization_predictions"],
+    }
+
+    def description(self):
+        return """
+        Detailed description coming soon...!
+        """
+
+    def run(self):
+        display_limit = self.params["display_limit"]
+
+        # Check model and dataset attributes
+        if not hasattr(self, "model"):
+            raise AttributeError("The 'model' attribute is missing.")
+
+        y_true = self.model.y_test_true
+        y_pred = self.model.y_test_predict
+        text_column = self.model.test_ds.text_column
+        text_input = self.model.test_ds.df[text_column]
+
+        # Ensure consistency in lengths
+        if not len(y_true) == len(y_pred) == len(text_input):
+            raise ValueError(
+                "Inconsistent lengths among input text, true summaries, and predicted summaries."
+            )
+
+        # Create a DataFrame with results and user-friendly column names
+        df = pd.DataFrame(
+            {
+                "Input Text": text_input,
+                "True Summaries": y_true.reshape(-1),
+                "Predicted Summaries": y_pred,
+            }
+        )
+
+        # Limit the number of rows to display based on the display_limit
+        df = df.head(display_limit)
+
+        return self.cache_results(df.to_dict(orient="records"))
+
+    def summary(self, metric_value):
+        df = pd.DataFrame(metric_value)
+
+        return ResultSummary(
+            results=[
+                ResultTable(data=df.to_dict(orient="records")),
+            ]
+        )
--- validmind/tests/model_validation/ToxicityHistogram.py
+++ validmind/tests/model_validation/ToxicityHistogram.py
@@ -15,21 +15,42 @@ class ToxicityHistogram(Metric):
     """
 
     name = "toxicity_histogram"
-    required_inputs = ["model", "model.test_ds"]
-    default_params = {"toxicity_obj": None}
+    default_params = {"text_columns": None}
 
     def description(self):
         return """
         Toxicity detailed description coming soon...!
         """
 
+    def _determine_source(self):
+        """
+        Determine the source based on the existence of attributes.
+        """
+
+        # Checking attributes if they exist
+        has_text_columns = (
+            hasattr(self, "params") and self.params.get("text_columns") is not None
+        )
+        has_dataset = hasattr(self, "dataset")
+        has_model = hasattr(self, "model.y_test_predict")
+
+        # Conditions based on comments
+        if not has_text_columns and not has_dataset and not has_model:
+            raise ValueError("Neither text_columns, dataset nor model exist!")
+
+        if not has_dataset and not has_model:
+            raise ValueError("Neither dataset nor model exist!")
+
+        # Determine the source
+        if has_text_columns:
+            return self.params["text_columns"]
+        elif has_model:
+            return (self.model.y_test_true, self.model.y_test_predict)
+        elif has_dataset:
+            return self.dataset
+
     def run(self):
-        toxicity_obj = self.params["toxicity_obj"]
-        if toxicity_obj is None:
-            toxicity_obj = evaluate.load("toxicity")
-
-        y_true = list(itertools.chain.from_iterable(self.model.y_test_true))
-        y_pred = self.model.y_test_predict
-
-        print(y_true)
-        print(y_pred)
+        source = self._determine_source()
+        # Use 'source' to compute toxicity
+        # For demonstration purposes, just printing it.
+        print(source)
