d942ade8057cc8cc0b0b0fb3bc6349fa43c84539
Anil Sorathiya <anil@validmind.ai>
2023-10-11 09:15:51 +0100

    add test description

--- validmind/tests/model_validation/sklearn/RegressionErrors.py
+++ validmind/tests/model_validation/sklearn/RegressionErrors.py
@@ -10,6 +10,34 @@ from validmind.vm_models import Metric, ResultSummary, ResultTable
 
 @dataclass
 class RegressionErrors(Metric):
+    """
+    **Purpose**: This metric is used to measure the performance of a regression model. It gauges the model's accuracy
+    by computing several error metrics such as Mean Absolute Error (MAE), Mean Squared Error (MSE), Root Mean Squared
+    Error (RMSE), Mean Absolute Percentage Error (MAPE), and Mean Bias Deviation (MBD) on both the training and testing
+    dataset.
+
+    **Test Mechanism**: The test computes each of the aforementioned metrics. MAE calculates the average of the
+    absolute difference between the true value and the predicted value. MSE squares the difference before averaging it.
+    RMSE then takes the square root of the MSE. MAPE evaluates the average of the absolute difference between true and
+    predicted values divided by the true value, expressed as a percentage. Lastly, MBD is a measure of average bias in
+    the prediction. The results are compared between the training dataset and the testing dataset.
+
+    **Signs of High Risk**: High values for any of the metrics, or particularly different metric outcomes for the
+    training set versus the test set, are signs of high risk. Specifically, high MAE, MSE, RMSE, or MAPE values could
+    indicate poor model performance and overfitting. If MBD is significantly different from zero, it could signify that
+    the model's predictions are systematically biased.
+
+    **Strengths**: These metrics collectively provide a comprehensive view of model performance and error distribution.
+    Individually, MAE provides a linear score that could be more interpretable, while MSE gives more weight to larger
+    errors. RMSE is useful because it is in the same unit as the target variable. MAPE expresses error as a percentage,
+    making it a good measure of prediction accuracy. MBD helps to detect systematic bias in predictions.
+
+    **Limitations**: Each of these metrics has its own limitations. MAE and MSE are sensitive to outliers. While RMSE
+    is good for giving high weight to larger errors, it might too heavily penalize these errors. MAPE might be biased
+    if actual values are near zero, and MBD would not work well if the difference between predictions and actual values
+    changes with the magnitude of the actual values. Overall, these metrics will not capture all model performance
+    nuances, and they should be used with contextual understanding of the problem at hand.
+    """
 
     category = "model_performance"
     name = "regression_errors"
--- validmind/tests/model_validation/sklearn/RegressionR2Square.py
+++ validmind/tests/model_validation/sklearn/RegressionR2Square.py
@@ -10,6 +10,34 @@ from validmind.vm_models import Metric, ResultSummary, ResultTable
 
 @dataclass
 class RegressionR2Square(Metric):
+    """
+    **Purpose**: The purpose of the RegressionR2Square Metric test is to measure the overall goodness-of-fit of a
+    regression model. Specifically, this Python-based test evaluates the R-squared (R2) and Adjusted R-squared (Adj R2)
+    scores: two statistical measures within regression analysis used to evaluate the strength of the relationship
+    between the model's predictors and the response variable.
+
+    **Test Mechanism**: The test deploys the 'r2_score' method from the Scikit-learn metrics module, measuring the R2
+    score on both training and test sets. This score reflects the proportion of the variance in the dependent variable
+    that is predictable from independent variables. The test also considers the Adjusted R2 score, accounting for the
+    number of predictors in the model, to penalize model complexity and thus reduce overfitting. The Adjusted R2 score
+    will be smaller if unnecessary predictors are included in the model.
+
+    **Signs of High Risk**: Indicators of high risk in this test may include a low R2 or Adjusted R2 score, which would
+    suggest that the model does not explain much variation in the dependent variable. The occurrence of overfitting is
+    also a high-risk sign, evident when the R2 score on the training set is significantly higher than on the test set,
+    indicating that the model is not generalizing well to unseen data.
+
+    **Strengths**: The R2 score is a widely-used measure in regression analysis, providing a sound general indication
+    of model performance. It is easy to interpret and understand, as it is essentially representing the proportion of
+    the dependent variable's variance explained by the independent variables. The Adjusted R2 score complements the R2
+    score well by taking into account the number of predictors in the model, which helps control overfitting.
+
+    **Limitations**: R2 and Adjusted R2 scores can be sensitive to the inclusion of unnecessary predictors in the model
+    (even though Adjusted R2 is intended to penalize complexity). Their reliability might also lessen in cases of
+    non-linear relationships or when the underlying assumptions of linear regression are violated. Additionally, while
+    they summarize how well the model fits the data, they do not provide insight on whether the correct regression was
+    used, or whether certain key assumptions have been fulfilled.
+    """
 
     category = "model_performance"
     name = "regression_errors"
