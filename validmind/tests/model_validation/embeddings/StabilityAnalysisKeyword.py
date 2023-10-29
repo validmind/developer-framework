4f098130b3178d4408e8feee3cfe95910e07a0cf
Andres Rodriguez <andres@validmind.ai>
2023-10-29 12:44:09 -0700

    Update descriptions, move notebook

--- validmind/tests/model_validation/embeddings/StabilityAnalysisKeyword.py
+++ validmind/tests/model_validation/embeddings/StabilityAnalysisKeyword.py
@@ -12,6 +12,38 @@ class StabilityAnalysisKeyword(StabilityAnalysis):
     This tests expects a parameter `keyword_dict` that maps words to other words
     so that any instances of the key words in the test dataset will be replaced
     with the corresponding value.
+
+    **Purpose:**
+    This test metric is used to evaluate the robustness of text embedding machine learning models to
+    keyword swaps. A keyword swap is a scenario where instances of certain specified keywords in the dataset are
+    replaced with other specified words (usually synonyms). The purpose of this metric is to ensure that these models
+    maintain performance stability even when the input data slightly deviates, imitating real-world variability.
+
+    **Test Mechanism:**
+    The test mechanism involves perturbation of the dataset used in testing the model. Each
+    instance of a specific word found in the dataset is replaced with the corresponding word as specified in a
+    'keyword_dict' mapping. The model is then re-run with the perturbed dataset and the results are compared with the
+    non-perturbed dataset. This comparison quantifies the extent to which keyword swaps impact the model's performance.
+
+    **Signs of High Risk:**
+    - A significant drop in model performance after keyword swaps indicates a high risk of model failure in real-world
+    scenarios.
+    - The model results being heavily reliant on specific word choices instead of capturing the context properly.
+
+    **Strengths:**
+    - This test provides a way to measure model robustness to small changes in input data, which reinforces its
+    applicability and reliability in real-world scenarios.
+    - This test encourages a model to understand the context of a sentence rather than memorizing specific words.
+    - It helps to detect overfitting - a situation where a model performs well on training data but poorly on new or
+    slightly altered data.
+
+    **Limitations:**
+    - It may not fully address semantic differences that can be introduced through keyword swaps. That is, the
+    replacement words might not preserve the exact semantic meaning of the original words.
+    - It only tests for changes in keywords (word-level alterations) and might not expose model limitations related to
+    structural data changes.
+    - It assumes that the provided 'keyword_dict' is an accurate representation of possible real-world variations,
+    which might not always be the case.
     """
 
     name = "Text Embeddings Stability Analysis to Keyword Swaps"
--- validmind/tests/model_validation/embeddings/StabilityAnalysisRandomNoise.py
+++ validmind/tests/model_validation/embeddings/StabilityAnalysisRandomNoise.py
@@ -61,16 +61,52 @@ class StabilityAnalysisRandomNoise(StabilityAnalysis):
     """
     Evaluate robustness of embeddings models to random noise introduced by using
     a `probability` parameter to choose random locations in the text to apply
-    random perturbations.
+    random perturbations. These perturbations include:
 
-    These perturbations include:
     - Swapping two adjacent words
     - Introducing a random typo in a word
     - Deleting a random word
     - Inserting a random word at a random position
 
-    The `probability` parameter determines the probability of applying a
-    perturbation at each `word` in the text.
+    **Purpose:**
+    The purpose of the stability analysis is to evaluate the robustness of a text embeddings model to random noise.
+    Random perturbations such as swapping adjacent words, introducing random typos, deleting random words, or inserting
+    random words at random positions in the text are introduced, to gauge the model's performance and stability.
+
+    **Test Mechanism:**
+    The test mechanism includes a series of function-defined random perturbations like swapping two words
+    `random_swap`, introducing a typo in a word `introduce_typo`, deleting a random word `random_deletion`, and
+    inserting a random word at a random position `random_insertion`. A probability parameter determines the likelihood
+    or frequency of these perturbations within the text data.
+
+    The `perturb_data` function initally tokenizes the string data based on spaces then applies selected random
+    perturbations based on the provided probability for each word in the text.
+
+    **Signs of High Risk:**
+    - High error rates in model predictions or classifications after the introduction of the random noise
+    - Greater sensitivity to certain types and degrees of noise such as typographical errors, insertion or deletion of
+    words
+    - Significant change in loss function or accuracy metric
+    - Inconsistency in model outputs for slightly perturbed inputs
+
+    **Strengths:**
+    - Measures model robustness against noise thereby reflecting real-world scenarios where data may contain errors or
+    inconsistencies.
+    - Easy to implement with adjustable perturbation severity through probability parameter.
+    - Enables identification of model sensitivity to certain noise types, providing grounding for model improvement.
+    - Useful for testing models designed to handle text data.
+
+    **Limitations:**
+    - The test might not be effective for models that have been designed with a high resistance to noise or models that
+    are inherently designed to handle such perturbations.
+    - Pseudo-randomness may not accurately represent real-world distribution of noise or typographical errors, which
+    could be biased towards certain types of errors or malware injections.
+    - Highly dependent on the probability parameter to introduce noise, with artificial adjusting required to achieve
+    an optimal balance.
+    - Only validates the model's performance against noise in input data, not its ability to capture complex language
+    structures or semantics.
+    - Does not guarantee the model's performance in new, unseen, real-world data beyond what is represented by the
+    noise-introduced test data.
     """
 
     name = "Text Embeddings Stability Analysis to Random Noise"
