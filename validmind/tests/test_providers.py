ca791b14981ddcc9083d42453fdb1c9ad8734b59
John Walz <john@validmind.ai>
2023-11-27 14:35:50 -0500

    Cannot load relative tests when using a test [sc-2467]

--- validmind/tests/test_providers.py
+++ validmind/tests/test_providers.py
@@ -2,8 +2,13 @@
 
 import importlib.util
 import os
+import sys
 
 import requests
+
+from validmind.logging import get_logger
+
+logger = get_logger(__name__)
 
 
 class GithubTestProviderDownloadError(Exception):
@@ -132,9 +137,9 @@ class GithubTestProvider:
         test_path = f"{test_id.replace('.', '/')}.py"
         file_path = self._download_file(test_path)
 
-        spec = importlib.util.spec_from_file_location(test_id, file_path)
-        module = importlib.util.module_from_spec(spec)
         try:
+            spec = importlib.util.spec_from_file_location(test_id, file_path)
+            module = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(module)
         except Exception as e:
             raise GithubTestProviderLoadModuleError(
@@ -152,12 +157,37 @@ class GithubTestProvider:
 
 class LocalTestProvider:
     """
-    A class used to load and execute tests from local Python files.
+    Test providers in ValidMind are responsible for loading tests from different sources,
+    such as local files, databases, or remote services. The LocalTestProvider specifically
+    loads tests from the local file system.
+
+    To use the LocalTestProvider, you need to provide the root_folder, which is the
+    root directory for local tests. The test_id is a combination of the namespace (set
+    when registering the test provider) and the path to the test class module, where
+    slashes are replaced by dots and the .py extension is left out.
+
+    Example usage:
+
+    ```
+    # Create an instance of LocalTestProvider with the root folder
+    test_provider = LocalTestProvider("/path/to/tests/folder")
+
+    # Register the test provider with a namespace
+    register_test_provider("my_namespace", test_provider)
+
+    # Load a test using the test_id (namespace + path to test class module)
+    test = test_provider.load_test("my_namespace.my_test_class")
+    # full path to the test class module is /path/to/tests/folder/my_test_class.py
+    ```
+
+    Attributes:
+        root_folder (str): The root directory for local tests.
     """
 
     def __init__(self, root_folder: str):
         """
-        Initialize the LocalTestProvider with the given root_folder.
+        Initialize the LocalTestProvider with the given root_folder
+        (see class docstring for details)
 
         Args:
             root_folder (str): The root directory for local tests.
@@ -169,8 +199,8 @@ class LocalTestProvider:
         Load the test identified by the given test_id.
 
         Args:
-            test_id (str): The identifier of the test. This corresponds to the
-            relative path of the python file from the root folder, with slashes replaced by dots.
+            test_id (str): The identifier of the test. This corresponds to the relative
+            path of the python file from the root folder, with slashes replaced by dots
 
         Returns:
             The test class that matches the last part of the test_id.
@@ -181,14 +211,40 @@ class LocalTestProvider:
         test_path = f"{test_id.replace('.', '/')}.py"
         file_path = os.path.join(self.root_folder, test_path)
 
-        spec = importlib.util.spec_from_file_location(test_id, file_path)
-        module = importlib.util.module_from_spec(spec)
-        try:
-            spec.loader.exec_module(module)
-        except Exception as e:
-            raise LocalTestProviderLoadModuleError(
-                f"Failed to load the module from {file_path}. Error: {str(e)}"
-            )
+        logger.debug(f"Loading test {test_id} from {file_path}")
+
+        # Check if the module uses relative imports
+        with open(file_path, "r") as file:
+            lines = file.readlines()
+
+        # handle test with relative imports
+        if any(line.strip().startswith("from .") for line in lines):
+            logger.debug("Found relative imports, using alternative import method")
+
+            parent_folder = os.path.dirname(file_path)
+            if parent_folder not in sys.path:
+                sys.path.insert(0, os.path.dirname(parent_folder))
+
+            try:
+                module = importlib.import_module(
+                    f"{os.path.basename(parent_folder)}.{test_id}"
+                )
+            except Exception as e:
+                # error will be handled/re-raised by `load_test` func
+                raise LocalTestProviderLoadModuleError(
+                    f"Failed to load the module from {file_path}. Error: {str(e)}"
+                )
+
+        else:
+            try:
+                spec = importlib.util.spec_from_file_location(test_id, file_path)
+                module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(module)
+            except Exception as e:
+                # error will be handled/re-raised by `load_test` func
+                raise LocalTestProviderLoadModuleError(
+                    f"Failed to load the module from {file_path}. Error: {str(e)}"
+                )
 
         try:
             # find the test class that matches the last part of the test_id
