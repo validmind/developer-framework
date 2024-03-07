6843608a11a4ec134eaf643eda4c6285f75573bc
Juan <juan@validmind.ai>
2024-03-07 21:16:57 +0000

    Add copyright

--- validmind/unit_metrics/__init__.py
+++ validmind/unit_metrics/__init__.py
@@ -1,3 +1,7 @@
+# Copyright © 2023-2024 ValidMind Inc. All rights reserved.
+# See the LICENSE file in the root of this repository for details.
+# SPDX-License-Identifier: AGPL-3.0 AND ValidMind Commercial
+
 import numpy as np
 import json
 import hashlib
