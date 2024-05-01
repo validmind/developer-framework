cdb3c9d67eb8ad61624ae2ea0977c77c95dfa9c9
John Walz <john@validmind.ai>
2024-05-01 00:19:24 -0400

    getting ragas metrics working (or at least an initial version)

--- validmind/tests/model_validation/ragas/AnswerSimilarity.py
+++ validmind/tests/model_validation/ragas/AnswerSimilarity.py
@@ -2,48 +2,12 @@
 # See the LICENSE file in the root of this repository for details.
 # SPDX-License-Identifier: AGPL-3.0 AND ValidMind Commercial
 
-from dataclasses import dataclass
-
 from datasets import Dataset
 from ragas import evaluate
 from ragas.metrics import answer_similarity
 
-from validmind.vm_models import Metric, ResultSummary, ResultTable
 
+def AnswerSimilarity(dataset):
+    result = evaluate(Dataset.from_pandas(dataset._df), metrics=[answer_similarity])
 
-@dataclass
-class AnswerSimilarity(Metric):
-
-    name = "answer_similarity"
-    required_inputs = ["model", "dataset"]
-    default_params = {
-        "question": "question",
-        "answer": "answer",
-        "ground_truth": "ground_truth",
-        "contexts": "contexts",
-    }
-
-    def summary(self, metric_value):
-        return ResultSummary(
-            results=[
-                ResultTable(data=metric_value),
-            ]
-        )
-
-    def run(self):
-        df = self.inputs.dataset.df
-        Dataset.from_pandas(df)
-        dataset = df[
-            [
-                self.params["question"],
-                self.params["answer"],
-                self.params["ground_truth"],
-                self.params["contexts"],
-            ]
-        ]
-        dataset = Dataset.from_pandas(dataset)
-        score = evaluate(dataset, metrics=[answer_similarity])
-        result = score.to_pandas()
-        result = result.drop(columns=["__index_level_0__"])
-
-        return self.cache_results(metric_value=result)
+    return result.to_pandas()["answer_similarity"].to_list()
