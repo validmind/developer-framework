c10f5f3896e9d1d28149a25da51fd616a851dccb
Anil Sorathiya <30263958+AnilSorathiya@users.noreply.github.com>
2024-05-08 17:11:41 +0100

    Fix notebook and ragas tests (#74)
    * Ragas tests fixes
    
    * clean the text output from notebook

--- validmind/tests/model_validation/ragas/AnswerSimilarity.py
+++ validmind/tests/model_validation/ragas/AnswerSimilarity.py
@@ -14,14 +14,16 @@ def AnswerSimilarity(
     ground_truth_column="ground_truth",
     contexts_column="contexts",
 ):
-    required_columns = [
-        question_column,
-        answer_column,
-        ground_truth_column,
-        contexts_column,
-    ]
+    required_columns = {
+        question_column: "question",
+        answer_column: "answer",
+        ground_truth_column: "ground_truth",
+        contexts_column: "contexts",
+    }
+    df = dataset.df.rename(columns=required_columns, inplace=False)
     result = evaluate(
-        Dataset.from_pandas(dataset.df[required_columns]), metrics=[answer_similarity]
+        Dataset.from_pandas(df[list(required_columns.values())]),
+        metrics=[answer_similarity],
     )
 
     return result.to_pandas()["answer_similarity"].to_list()
