dca4ccdc0b40450208e80d3b6282b8740d657c70
Anil Sorathiya <anil@validmind.ai>
2024-05-10 15:58:09 +0100

    add more ragas metrics

---
+++ validmind/tests/model_validation/ragas/AnswerCorrectness.py
@@ -0,0 +1,61 @@
+# Copyright © 2023-2024 ValidMind Inc. All rights reserved.
+# See the LICENSE file in the root of this repository for details.
+# SPDX-License-Identifier: AGPL-3.0 AND ValidMind Commercial
+
+import plotly.express as px
+from datasets import Dataset
+from ragas import evaluate
+from ragas.metrics import answer_correctness
+
+from validmind import tags, tasks
+
+
+@tags("nlp", "text_data", "visualization")
+@tasks("text_classification", "text_summarization")
+def AnswerCorrectness(
+    dataset,
+    question_column="question",
+    answer_column="answer",
+    ground_truth_column="ground_truth",
+    contexts_column="contexts",
+):
+    """
+    Evaluates the correctness of answers in a dataset with respect to the provided ground truths and
+    visualizes the results in a histogram.
+
+    This function calculates the correctness of answers by comparing them to the ground truth references
+    within the context of the given questions and contexts. It utilizes a pre-defined correctness metric
+    to assess these comparisons and generates a histogram plot of the correctness scores.
+
+    Args:
+        dataset (Dataset): A dataset object which must have a `df` attribute (a pandas DataFrame)
+            containing the necessary columns.
+        question_column (str, optional): The name of the column containing questions. Defaults to "question".
+        answer_column (str, optional): The name of the column containing answers. Defaults to "answer".
+        ground_truth_column (str, optional): The name of the column containing the correct answers
+            or reference text. Defaults to "ground_truth".
+        contexts_column (str, optional): The name of the column containing the contexts related to each
+            question and answer pair. Defaults to "contexts".
+
+    Returns:
+        plotly.graph_objs._figure.Figure: A Plotly histogram plot showing the distribution of answer correctness
+        scores across the dataset's entries.
+
+    Raises:
+        KeyError: If any of the required columns are missing in the dataset.
+    """
+    required_columns = {
+        question_column: "question",
+        answer_column: "answer",
+        ground_truth_column: "ground_truth",
+        contexts_column: "contexts",
+    }
+    df = dataset.df.rename(columns=required_columns, inplace=False)
+    result = evaluate(
+        Dataset.from_pandas(df[list(required_columns.values())]),
+        metrics=[answer_correctness],
+    )
+
+    fig = px.histogram(x=result.to_pandas()["answer_correctness"].to_list(), nbins=10)
+
+    return fig
