147731b26eb9169e61366cf4c780b716e90ef3ef
Anil Sorathiya <30263958+AnilSorathiya@users.noreply.github.com>
2024-05-10 14:49:13 +0100

    add test description (#78)

--- validmind/tests/data_validation/nlp/LanguageDetection.py
+++ validmind/tests/data_validation/nlp/LanguageDetection.py
@@ -16,7 +16,26 @@ from validmind import tags, tasks
 @tags("nlp", "text_data", "visualization")
 @tasks("text_classification", "text_summarization")
 def LanguageDetection(dataset):
+    """
+    Detects the language of each text entry in a dataset and visualizes the distribution of languages
+    as a histogram.
 
+    This method checks for a specified text column in the dataset's dataframe, uses a language detection
+    library to determine the language of each text entry, and returns a histogram plot of the language
+    distribution.
+
+    Args:
+        dataset (Dataset): A dataset object which must have a `df` attribute (a pandas DataFrame)
+            and a `text_column` attribute indicating the name of the column containing text. If the
+            `text_column` attribute is not set, a ValueError is raised.
+
+    Returns:
+        plotly.graph_objs._figure.Figure: A Plotly histogram plot showing the distribution of detected
+        languages across the dataset's text entries.
+
+    Raises:
+        ValueError: If the `text_column` is not specified in the dataset object.
+    """
     # check text column
     if not dataset.text_column:
         raise ValueError("Please set text_column name in the Validmind Dataset object")
