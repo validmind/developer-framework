c9719d6fee000c9dbd93686e93bacd54a7239b4a
John Walz <john@validmind.ai>
2024-05-14 19:32:04 -0400

    fix linter error

--- validmind/template.py
+++ validmind/template.py
@@ -2,8 +2,6 @@
 # See the LICENSE file in the root of this repository for details.
 # SPDX-License-Identifier: AGPL-3.0 AND ValidMind Commercial
 
-from pprint import pformat
-
 from ipywidgets import HTML, Accordion, VBox
 
 from .html_templates.content_blocks import (
