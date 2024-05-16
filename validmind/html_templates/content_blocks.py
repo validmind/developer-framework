a61329fc62cbef9c654deba4059db2d8771e6682
John Walz <john@validmind.ai>
2024-05-15 22:34:39 -0400

    forgot a end script tag

--- validmind/html_templates/content_blocks.py
+++ validmind/html_templates/content_blocks.py
@@ -127,6 +127,7 @@ setTimeout(function () {
     script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-AMS_HTML';
     document.head.appendChild(script);
 }, 300);
+</script>
 """
 
 failed_content_block_html = """
