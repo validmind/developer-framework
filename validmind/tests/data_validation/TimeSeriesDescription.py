0b8c9fb93426920b13d7bc3c2019d4fa8703f7dd
Juan <juan@validmind.ai>
2024-06-11 17:45:53 +0200

    Add copyright

--- validmind/tests/data_validation/TimeSeriesDescription.py
+++ validmind/tests/data_validation/TimeSeriesDescription.py
@@ -1,3 +1,7 @@
+# Copyright © 2023-2024 ValidMind Inc. All rights reserved.
+# See the LICENSE file in the root of this repository for details.
+# SPDX-License-Identifier: AGPL-3.0 AND ValidMind Commercial
+
 import pandas as pd
 
 from validmind import tags, tasks
