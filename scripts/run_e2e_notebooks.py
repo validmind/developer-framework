8262d4ea50af45615aad2eb2fd99e8f74f60c49b
Juan <juan@validmind.ai>
2024-06-13 14:17:52 +0200

    Replace time series notebook in integration tests

--- scripts/run_e2e_notebooks.py
+++ scripts/run_e2e_notebooks.py
@@ -42,7 +42,7 @@ NOTEBOOKS_TO_RUN = [
         "path": "notebooks/code_samples/quickstart_customer_churn_full_suite.ipynb",
         "project": DEFAULT_PROJECT_ID,
     },
-    "notebooks/code_samples/time_series/tutorial_time_series_forecasting.ipynb",
+    "notebooks/code_samples/time_series/quickstart_time_series_full_suite.ipynb",
     # "notebooks/code_samples/regression/quickstart_regression_full_suite.ipynb",
     # TODO: fix the above when we have a regression template installed
     "notebooks/how_to/run_unit_metrics.ipynb",
