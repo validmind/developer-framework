849bab5ff0b7b9fd6e5bfc6572548860bada17b2
Andres Rodriguez <andres@validmind.ai>
2024-08-26 14:11:20 -0700

    Refactor add_test_description script (#178)
    * Refactor add_test_description script
    
    * Added instructions to README
    
    * Omg I figured it out
    
    * I fixed the purpose & test mechanism wonky formatting
    
    * Trailing whitespace?? I think I removed it
    
    * Trying again melting_face
    
    * 2.5.10
    
    ---------
    
    Co-authored-by: Beck <164545837+validbeck@users.noreply.github.com>

--- validmind/tests/ongoing_monitoring/PredictionAcrossEachFeature.py
+++ validmind/tests/ongoing_monitoring/PredictionAcrossEachFeature.py
@@ -12,31 +12,37 @@ from validmind import tags, tasks
 @tasks("monitoring")
 def PredictionAcrossEachFeature(datasets, model):
     """
-    **Purpose:**
-    This test shows visually the prediction using reference data and monitoring data across each individual feature. If
-    there are significant differences in predictions across feature values from reference to monitoring dataset, then
-    further investigation is needed as the model is producing predictions that are different than what was observed
-    during the training of the model.
+    Assesses differences in model predictions across individual features between reference and monitoring datasets
+    through visual analysis.
 
-    **Test Mechanism:**
-    The test creates scatter plots for each feature, comparing the reference dataset (used for training) with the
-    monitoring dataset (used in production). Each plot has two subplots: one for the reference data and one for the
-    monitoring data, visualizing the prediction probabilities. This allows for a visual comparison of the model's
-    behavior across different datasets.
+    ### Purpose
 
-    **Signs of High Risk:**
-    - Significant discrepancies between the reference and monitoring subplots for the same feature
-    - Unexpected patterns or trends in monitoring data that weren't present in reference data
+    The Prediction Across Each Feature test aims to visually compare model predictions for each feature between
+    reference (training) and monitoring (production) datasets. It helps identify significant differences in prediction
+    patterns for further investigation and ensures the model's consistency and stability over time.
 
-    **Strengths:**
-    - Provides a clear visual representation of model performance across different features
-    - Allows for easy identification of features where the model's predictions have changed
-    - Facilitates quick detection of potential issues with the model when deployed in production
+    ### Test Mechanism
 
-    **Limitations:**
-    - Interpretation of scatter plots can be subjective and may require expertise
-    - Visualizations do not provide quantitative metrics for objective evaluation
-    - May not capture all types of distribution changes or issues with the model's predictions
+    The test generates scatter plots for each feature, comparing prediction probabilities between the reference and
+    monitoring datasets. Each plot consists of two subplots: one for reference data and one for monitoring data,
+    enabling visual comparison of the model's predictive behavior.
+
+    ### Signs of High Risk
+
+    - Significant discrepancies between the reference and monitoring subplots for the same feature.
+    - Unexpected patterns or trends in monitoring data that were absent in reference data.
+
+    ### Strengths
+
+    - Provides a clear visual representation of model performance across different features.
+    - Facilitates easy identification of features where the model's predictions have diverged.
+    - Enables quick detection of potential model performance issues in production.
+
+    ### Limitations
+
+    - Interpretation of scatter plots can be subjective and may require expertise.
+    - Visualizations do not provide quantitative metrics for objective evaluation.
+    - May not capture all types of distribution changes or issues with the model's predictions.
     """
 
     """
