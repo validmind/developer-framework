2c95a6e12929fe3b009a7564f19fa5926eb044b1
Beck <164545837+validbeck@users.noreply.github.com>
2024-08-27 10:15:44 -0700

    Reran model_validation/sklearn up to P

--- validmind/tests/model_validation/sklearn/CompletenessScore.py
+++ validmind/tests/model_validation/sklearn/CompletenessScore.py
@@ -14,26 +14,32 @@ class CompletenessScore(ClusterPerformance):
     """
     Evaluates a clustering model's capacity to categorize instances from a single class into the same cluster.
 
-    **Purpose:** The Completeness Score metric is used to assess the performance of clustering models. It measures the
-    extent to which all the data points that are members of a given class are elements of the same cluster. The aim is
-    to determine the capability of the model to categorize all instances from a single class into the same cluster.
+    ### Purpose
 
-    **Test Mechanism:** This test takes three inputs, a model and its associated training and testing datasets. It
-    invokes the `completeness_score` function from the sklearn library on the labels predicted by the model. High
-    scores indicate that data points from the same class generally appear in the same cluster, while low scores suggest
-    the opposite.
+    The Completeness Score metric is used to assess the performance of clustering models. It measures the extent to
+    which all the data points that are members of a given class are elements of the same cluster. The aim is to
+    determine the capability of the model to categorize all instances from a single class into the same cluster.
 
-    **Signs of High Risk:**
+    ### Test Mechanism
+
+    This test takes three inputs, a model and its associated training and testing datasets. It invokes the
+    `completeness_score` function from the sklearn library on the labels predicted by the model. High scores indicate
+    that data points from the same class generally appear in the same cluster, while low scores suggest the opposite.
+
+    ### Signs of High Risk
+
     - Low completeness score: This suggests that the model struggles to group instances from the same class into one
     cluster, indicating poor clustering performance.
 
-    **Strengths:**
+    ### Strengths
+
     - The Completeness Score provides an effective method for assessing the performance of a clustering model,
     specifically its ability to group class instances together.
     - This test metric conveniently relies on the capabilities provided by the sklearn library, ensuring consistent and
     reliable test results.
 
-    **Limitations:**
+    ### Limitations
+
     - This metric only evaluates a specific aspect of clustering, meaning it may not provide a holistic or complete
     view of the model's performance.
     - It cannot assess the effectiveness of the model in differentiating between separate classes, as it is solely
--- validmind/tests/model_validation/sklearn/MinimumAccuracy.py
+++ validmind/tests/model_validation/sklearn/MinimumAccuracy.py
@@ -22,38 +22,38 @@ class MinimumAccuracy(ThresholdTest):
     """
     Checks if the model's prediction accuracy meets or surpasses a specified threshold.
 
-    **Purpose**: The Minimum Accuracy test’s objective is to verify whether the model's prediction accuracy on a
-    specific dataset meets or surpasses a predetermined minimum threshold. Accuracy, which is simply the ratio of right
-    predictions to total predictions, is a key metric for evaluating the model's performance. Considering binary as
-    well as multiclass classifications, accurate labeling becomes indispensable.
+    ### Purpose
 
-    **Test Mechanism**: The test mechanism involves contrasting the model's accuracy score with a pre-set minimum
-    threshold value, default value being 0.7. The accuracy score is computed utilizing sklearn’s `accuracy_score`
-    method, where the true label `y_true` and predicted label `class_pred` are compared. If the accuracy score is above
-    the threshold, the test gets a passing mark. The test returns the result along with the accuracy score and
-    threshold used for the test.
+    The Minimum Accuracy test’s objective is to verify whether the model's prediction accuracy on a specific dataset
+    meets or surpasses a predetermined minimum threshold. Accuracy, which is simply the ratio of correct predictions to
+    total predictions, is a key metric for evaluating the model's performance. Considering binary as well as multiclass
+    classifications, accurate labeling becomes indispensable.
 
-    **Signs of High Risk**:
-    - The risk level for this test surges considerably when the model is unable to achieve or surpass the predefined
-    score threshold.
-    - When the model persistently scores below the threshold, it suggests a high risk of inaccurate predictions, which
-    in turn affects the model’s efficiency and reliability.
+    ### Test Mechanism
 
-    **Strengths**:
-    - One of the key strengths of this test is its simplicity, presenting a straightforward measure of the holistic
-    model performance across all classes.
-    - This test is particularly advantageous when classes are balanced.
-    - Another advantage of this test is its versatility as it can be implemented on both binary and multiclass
-    classification tasks.
+    The test mechanism involves contrasting the model's accuracy score with a preset minimum threshold value, with the
+    default being 0.7. The accuracy score is computed utilizing sklearn’s `accuracy_score` method, where the true
+    labels `y_true` and predicted labels `class_pred` are compared. If the accuracy score is above the threshold, the
+    test receives a passing mark. The test returns the result along with the accuracy score and threshold used for the
+    test.
 
-    **Limitations**:
-    - When analyzing imbalanced datasets, certain limitations of this test emerge. The accuracy score can be misleading
-    when classes in the dataset are skewed considerably.
-    - This can result in favoritism towards the majority class, consequently giving an inaccurate perception of the
-    model performance.
-    - Another limitation is its inability to measure the model's precision, recall, or capacity to manage false
-    positives or false negatives.
-    - The test majorly focuses on overall correctness and may not be sufficient for all types of model analytics.
+    ### Signs of High Risk
+
+    - Model fails to achieve or surpass the predefined score threshold.
+    - Persistent scores below the threshold, indicating a high risk of inaccurate predictions.
+      
+    ### Strengths
+
+    - Simplicity, presenting a straightforward measure of holistic model performance across all classes.
+    - Particularly advantageous when classes are balanced.
+    - Versatile, as it can be implemented on both binary and multiclass classification tasks.
+
+    ### Limitations
+
+    - Misleading accuracy scores when classes in the dataset are highly imbalanced.
+    - Favoritism towards the majority class, giving an inaccurate perception of model performance.
+    - Inability to measure the model's precision, recall, or capacity to manage false positives or false negatives.
+    - Focused on overall correctness and may not be sufficient for all types of model analytics.
     """
 
     name = "accuracy_score"
