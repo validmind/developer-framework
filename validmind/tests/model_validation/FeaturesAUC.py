f1ad0f2b44c2248e8e5e48eb4422bea84debaff5
Beck <164545837+validbeck@users.noreply.github.com>
2024-08-27 11:22:45 -0700

    Missed some in model_validation

--- validmind/tests/model_validation/FeaturesAUC.py
+++ validmind/tests/model_validation/FeaturesAUC.py
@@ -19,24 +19,43 @@ logger = get_logger(__name__)
 @dataclass
 class FeaturesAUC(Metric):
     """
-    Evaluates the discriminatory power of each individual feature within a binary classification model by calculating the Area Under the Curve (AUC) for each feature separately.
+    Evaluates the discriminatory power of each individual feature within a binary classification model by calculating
+    the Area Under the Curve (AUC) for each feature separately.
 
-    **Purpose**: The central objective of this metric is to quantify how well each feature on its own can differentiate between the two classes in a binary classification problem. It serves as a univariate analysis tool that can help in pre-modeling feature selection or post-modeling interpretation.
+    ### Purpose
 
-    **Test Mechanism**: For each feature, the metric treats the feature values as raw scores to compute the AUC against the actual binary outcomes. It provides an AUC value for each feature, offering a simple yet powerful indication of each feature's univariate classification strength.
+    The central objective of this metric is to quantify how well each feature on its own can differentiate between the
+    two classes in a binary classification problem. It serves as a univariate analysis tool that can help in
+    pre-modeling feature selection or post-modeling interpretation.
 
-    **Signs of High Risk**:
-    - A feature with a low AUC score may not be contributing significantly to the differentiation between the two classes, which could be a concern if it is expected to be predictive.
-    - Conversely, a surprisingly high AUC for a feature not believed to be informative may suggest data leakage or other issues with the data.
+    ### Test Mechanism
 
-    **Strengths**:
-    - By isolating each feature, it highlights the individual contribution of features to the classification task without the influence of other variables.
-    - Useful for both initial feature evaluation and for providing insights into the model's reliance on individual features after model training.
+    For each feature, the metric treats the feature values as raw scores to compute the AUC against the actual binary
+    outcomes. It provides an AUC value for each feature, offering a simple yet powerful indication of each feature's
+    univariate classification strength.
 
-    **Limitations**:
-    - Does not reflect the combined effects of features or any interaction between them, which can be critical in certain models.
-    - The AUC values are calculated without considering the model's use of the features, which could lead to different interpretations of feature importance when considering the model holistically.
-    - This metric is applicable only to binary classification tasks and cannot be directly extended to multiclass classification or regression without modifications.
+    ### Signs of High Risk
+
+    - A feature with a low AUC score may not be contributing significantly to the differentiation between the two
+    classes, which could be a concern if it is expected to be predictive.
+    - Conversely, a surprisingly high AUC for a feature not believed to be informative may suggest data leakage or
+    other issues with the data.
+
+    ### Strengths
+
+    - By isolating each feature, it highlights the individual contribution of features to the classification task
+    without the influence of other variables.
+    - Useful for both initial feature evaluation and for providing insights into the model's reliance on individual
+    features after model training.
+
+    ### Limitations
+
+    - Does not reflect the combined effects of features or any interaction between them, which can be critical in
+    certain models.
+    - The AUC values are calculated without considering the model's use of the features, which could lead to different
+    interpretations of feature importance when considering the model holistically.
+    - This metric is applicable only to binary classification tasks and cannot be directly extended to multiclass
+    classification or regression without modifications.
     """
 
     name = "features_auc"
--- validmind/tests/model_validation/ModelPredictionResiduals.py
+++ validmind/tests/model_validation/ModelPredictionResiduals.py
@@ -15,28 +15,38 @@ def ModelPredictionResiduals(
     datasets, models, nbins=100, p_value_threshold=0.05, start_date=None, end_date=None
 ):
     """
-    Plot the residuals and histograms for each model, and generate a summary table
-    with the Kolmogorov-Smirnov normality test results.
+    Assesses normality and behavior of residuals in regression models through visualization and statistical tests.
 
-    **Purpose**: The purpose of this function is to visualize the residuals of model predictions and
-    assess the normality of residuals using the Kolmogorov-Smirnov test.
+    ### Purpose
 
-    **Test Mechanism**: The function iterates through each dataset-model pair, calculates residuals, and generates
-    two figures for each model: one for the time series of residuals and one for the histogram of residuals.
-    It also calculates the KS test for normality and summarizes the results in a table.
+    The Model Prediction Residuals test aims to visualize the residuals of model predictions and assess their normality
+    using the Kolmogorov-Smirnov (KS) test. It helps to identify potential issues related to model assumptions and
+    effectiveness.
 
-    **Signs of High Risk**:
-    - If the residuals are not normally distributed, it could indicate issues with model assumptions.
-    - High skewness or kurtosis in the residuals may indicate model misspecification.
+    ### Test Mechanism
 
-    **Strengths**:
-    - Provides a clear visualization of residuals over time and their distribution.
+    This test involves the following steps for each dataset-model pair:
+
+    - Calculate the residuals by subtracting predicted values from the actual values.
+    - Generate two types of plots: a time series plot of residuals and a histogram of residuals.
+    - Perform the KS test to assess the normality of the residuals and summarize the results in a table.
+
+    ### Signs of High Risk
+
+    - Residuals are not normally distributed, indicating potential issues with model assumptions.
+    - High skewness or kurtosis in the residuals, which may suggest model misspecification.
+
+    ### Strengths
+
+    - Provides clear visualizations of residuals over time and their distribution.
     - Includes statistical tests to assess the normality of residuals.
+    - Helps in identifying potential model misspecifications and assumption violations.
 
-    **Limitations**:
-    - Assumes that the dataset is provided as a DataFrameDataset object with a .df attribute to access
-      the pandas DataFrame.
-    - Only generates plots for datasets with a datetime index, and will raise an error for other types of indices.
+    ### Limitations
+
+    - Assumes that the dataset is provided as a DataFrameDataset object with a .df attribute to access the pandas
+    DataFrame.
+    - Only generates plots for datasets with a datetime index, resulting in errors for other types of indices.
     """
 
     figures = []
