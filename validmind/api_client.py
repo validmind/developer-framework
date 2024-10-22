6e5eacc7aa5a50a6f08ad53409e8e7102dbabb4b
Cullen Taylor <cullen@eggshell.me>
2024-10-22 10:44:50 -0400

    temp: raise total timeout
    Signed-off-by: Cullen Taylor <cullen@eggshell.me>

--- validmind/api_client.py
+++ validmind/api_client.py
@@ -78,7 +78,7 @@ def _get_session() -> aiohttp.ClientSession:
     if not __api_session or __api_session.closed:
         __api_session = aiohttp.ClientSession(
             headers=_get_api_headers(),
-            timeout=aiohttp.ClientTimeout(total=30),
+            timeout=aiohttp.ClientTimeout(total=60),
         )
 
     return __api_session
